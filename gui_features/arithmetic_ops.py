d98abd05a51d2ad2b6897ff248c6b4707d0eb29e
kravago3 <kravago3@gatech.edu>
2021-01-09 19:56:40 -0800

    arithmetic ops notes - added bitwise ops

--- gui_features/arithmetic_ops.py
+++ gui_features/arithmetic_ops.py
@@ -1,17 +1,53 @@
 import cv2 
 
+# BLENDING IMAGES
+def blending_images():
+    img1 = cv2.imread('../media/scared_hamster.jpg')
+    img2 = cv2.imread('../media/vietnam.jpg')
 
-img1 = cv2.imread('../media/scared_hamster.jpg')
-img2 = cv2.imread('../media/vietnam.jpg')
+    # You can add two images together using cv2.add() or numpy operations but use the add method to avoid modulo operations
+    # Adding two images together with blended weights will result in a blended image. You can do this with cv2.addWeighted() method. Make sure the images are the same size
 
-# You can add two images together using cv2.add() or numpy operations but use the add method to avoid modulo operations
-# Adding two images together with blended weights will result in a blended image. You can do this with cv2.addWeighted() method. Make sure the images are the same size
+    # dst = cv2.addWeighted(img1, 0.7, img2, 0.3, 0)
+    dst = cv2.addWeighted(img1[:500, :500], 0.7, img2[:500, :500], 0.3, 0)
 
-# dst = cv2.addWeighted(img1, 0.7, img2, 0.3, 0)
-dst = cv2.addWeighted(img1[:500, :500], 0.7, img2[:500, :500], 0.3, 0)
+    # cv2.imwrite('../media/vietnamese_hamster.jpg', dst)
 
-# cv2.imwrite('../media/vietnamese_hamster.jpg', dst)
+    cv2.imshow('dst', dst)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-cv2.imshow('dst', dst)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+def bitwise_images():
+    '''Places logo on target image'''
+
+    img1 = cv2.imread('../media/robots.jpg')
+    img2 = cv2.imread('../media/opencv.png')
+
+    # create ROI in target image for logo
+    rows, cols, channels = img2.shape
+    roi = img1[:rows, :cols]
+
+    # create mask of logo and inverse mask
+    img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+    mask_inv = cv2.bitwise_not(mask)
+
+    # black out areas of the logo in ROI
+    img1_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+
+    # take only region of the logo from logo image
+    img2_fg = cv2.bitwise_and(img2, img2, mask=mask)
+
+    # put logo in ROI and modify the main image
+    dst = cv2.add(img1_bg, img2_fg)
+    img1[0:rows, 0:cols] = dst
+
+    images = [mask, mask_inv, img1_bg, img2_fg, dst]
+    for img in images:
+        cv2.imshow('res', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    bitwise_images()
